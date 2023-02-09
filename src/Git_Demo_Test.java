a115df560615bbb415e35dea6fb3874fea20aaf0
LuXun <LX2835969813@163.com>
2023-02-09 22:31:26 +0800

    git_demo_02

--- src/Git_Demo_Test.java
+++ src/Git_Demo_Test.java
@@ -7,5 +7,6 @@
 public class Git_Demo_Test {
     public static void main(String[] args) {
         System.out.println("Hello git_name_01");
+        System.out.println("Hello git_name_02");
     }
 }
